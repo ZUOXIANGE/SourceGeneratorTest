1263c33fc42ded95e195aa234b48726b21e9d209
zx <zuoxiang42@gmail.com>
2026-02-11 16:58:58 +0800

    chore: add husky

--- SourceGeneratorTest/Controllers/MediatorController.cs
+++ SourceGeneratorTest/Controllers/MediatorController.cs
@@ -123,6 +123,7 @@ public partial class MediatorController : ControllerBase
 public class BroadcastRequest
 {
     public Guid ProductId { get; set; }
+
     public string Name { get; set; } = string.Empty;
     public decimal Price { get; set; }
 }
